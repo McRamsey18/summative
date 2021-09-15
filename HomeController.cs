c02593243df8c004d3351fe9ce2ae0a12234f351
McRamsey18 <lethlokoramsey@gmail.com>
2021-09-15 21:01:44 +0200

    Create HomeController.cs

---
+++ HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Unika.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult Services()
+        {
+            return View();
+        }
+
+        public ActionResult Works()
+        {
+            return View();
+        }
+        public ActionResult Team()
+        {
+            return View();
+        }
+        public ActionResult Prices()
+        {
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
